8bcc6ea480c93520d625fc6a7bca7b3a73e78765
Robbie Bart <adm@AdminisatorsAir.lan>
2020-07-07 16:17:26 -0400

    mvp

--- public/order-form/scripts.js
+++ public/order-form/scripts.js
@@ -1,38 +1,38 @@
 const orderItems = {
-  undefined: { label: 'Pick an item', imgUrl: './assets/question.jpg' },
-  bottle: { label: 'Bottle', imgUrl: './assets/bottle.png' },
-  shirt: { label: 'T-shirt', imgUrl: './assets/tshirt.png' },
-  socks: { label: 'Socks', imgUrl: './assets/socks.jpg' },
+  undefined: { label: "Pick an item", imgUrl: "./assets/question.jpg" },
+  bottle: { label: "Bottle", imgUrl: "./assets/bottle.png" },
+  shirt: { label: "T-shirt", imgUrl: "./assets/tshirt.png" },
+  socks: { label: "Socks", imgUrl: "./assets/socks.jpg" },
 };
 const errorMessages = {
-  unavailable: 'Item out of stock. :(',
-  'repeat-customer':
-    'Exiting user. Not allowed to place another order at the moment.',
-  undeliverable: 'Outside of delivery zone. :(',
-  'missing-data': "Oops! Looks like we're missing some information.",
+  unavailable: "Item out of stock. :(",
+  "repeat-customer":
+    "Exiting user. Not allowed to place another order at the moment.",
+  undeliverable: "Outside of delivery zone. :(",
+  "missing-data": "Oops! Looks like we're missing some information.",
 };
-const submitButton = document.getElementById('confirm-button');
-const order = document.getElementById('order');
-const errorMsg = document.getElementById('error');
-const size = document.getElementById('size');
-const givenName = document.getElementById('givenName');
-const surname = document.getElementById('surname');
-const email = document.getElementById('email');
-const address = document.getElementById('address');
-const city = document.getElementById('city');
-const province = document.getElementById('province');
-const postcode = document.getElementById('postcode');
-const country = document.getElementById('country');
+const submitButton = document.getElementById("confirm-button");
+const order = document.getElementById("order");
+const errorMsg = document.getElementById("error");
+const size = document.getElementById("size");
+const givenName = document.getElementById("givenName");
+const surname = document.getElementById("surname");
+const email = document.getElementById("email");
+const address = document.getElementById("address");
+const city = document.getElementById("city");
+const province = document.getElementById("province");
+const postcode = document.getElementById("postcode");
+const country = document.getElementById("country");
 
 const updateForm = () => {
-  const sel = document.getElementById('order').value;
-  if (sel === 'shirt') {
-    document.getElementById('sizing').style.display = 'flex';
+  const sel = document.getElementById("order").value;
+  if (sel === "shirt") {
+    document.getElementById("sizing").style.display = "flex";
   } else {
-    document.getElementById('sizing').style.display = 'none';
+    document.getElementById("sizing").style.display = "none";
   }
   document.getElementById(
-    'order-image'
+    "order-image"
   ).style.backgroundImage = `url(${orderItems[sel].imgUrl}`;
 };
 
@@ -40,16 +40,16 @@ const handleToggleErrorMessage = (errorStatus) => {};
 
 const handleSubmit = (event) => {
   event.preventDefault(); //prevents default submit default of form (refresh the page)
-  console.log(size.value);
-  submitButton.disabled = true; 
-// at this point we've made sure that all default behaviours are stopped for a while
-// so that we can collect data from the form itself to prep it to send to the backend
+  // console.log(size.value);
+  submitButton.disabled = true;
+  // at this point we've made sure that all default behaviours are stopped for a while
+  // so that we can collect data from the form itself to prep it to send to the backend
   const data = {
     order: order.value,
     size: size.value,
     givenName: givenName.value,
     surname: surname.value,
-    email: email,
+    email: email.value,
     address: address.value,
     city: city.value,
     province: province.value,
@@ -60,23 +60,24 @@ const handleSubmit = (event) => {
 here we've collected data from form and put it all in a dataobject; at this point
 we're ready to send data to back end
   */
-  fetch('/order', {
-    method: 'POST',
+  fetch("/order", {
+    method: "POST",
     body: JSON.stringify(data),
     headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
+      Accept: "application/json",
+      "Content-Type": "application/json",
     },
   })
-    .then((res) => res.json()) 
+    .then((res) => res.json())
     .then((data) => {
       console.log("this is backend data", data);
       const { status, error } = data;
-      if (status === 'success') {
-        window.location.href = '/order-confirmed';
+      if (status === "success") {
+        window.location.href = "/order-confirmed";
+        // remove this and rerender
       } else if (error) {
         submitButton.disabled = false;
-        errorMsg.style.display = 'flex';
+        errorMsg.style.display = "flex";
         errorMsg.innerText = errorMessages[error];
       }
     });
--- server.js
+++ server.js
@@ -1,51 +1,87 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
 
 const { stock, customers } = require("./data/promo");
 
 express()
   .use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Origin", "*");
     res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept'
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
     );
     next();
   })
-  .use(morgan('tiny'))
-  .use(express.static('public'))
+  .use(morgan("tiny"))
+  .use(express.static("public"))
   .use(bodyParser.json())
   .use(express.urlencoded({ extended: false }))
-  .set('view engine', 'ejs')
+  .set("view engine", "ejs")
 
   // endpoints
   .post("/order", (req, res) => {
     const {
-        order,
-        size,
-        givenName,
-        surname,
-        email,
-        address,
-        city,
-        province,
-        postcode,
-        country,
-    } = req.body; 
-    if (stock.bottles === 0 || stock.socks === 0 || )
-    if (country !== "Canada") {
-      res.json({'status': 'error', 'error': 'underliverable'});
-    } res.json({status: "success"})
+      order,
+      size,
+      givenName,
+      surname,
+      email,
+      address,
+      city,
+      province,
+      postcode,
+      country,
+    } = req.body;
+
+    let response = { status: "success" };
+
+    if (order === "bottles") {
+      if (stock.bottles < 1) {
+        response = { status: "error", error: "unavailable" };
+      }
+    }
+
+    else if (order === "socks") {
+      if (stock.socks < 1) {
+        response = { status: "error", error: "unavailable" };
+      }
+    }
+
+    else if (order === "shirt") {
+      if (stock.shirt["size"] < 1) {
+        response = { status: "error", error: "unavailable" };
+      }
+    }
+
+    else if (country !== "Canada") {
+      response = { status: "error", error: "underliverable" };
+    }
+
+    // if (customers.find((cust) =>  cust.email === email)) {
+    //   res.json({ status: "error", error: "repeat-customer"});
+    // }
+
+    if (
+      customers.find((customer) => {
+        return customer.email === email;
+      })
+    ) {
+        response = ({ status: "error", error: "repeat-customer" });
+    }
+    
+    res.json(response);
   })
 
-  .get('*', (req, res) => res.send('Dang. 404.'))
+  .get("*", (req, res) => res.send("Dang. 404."))
   .listen(8000, () => console.log(`Listening on port 8000`));
 
-  /*
+  // .get('/order-confirmed', (req, res) => {
+  //   res.render("/order-confirmed.html");
+  // })
+/*
 we will now destructuring, which does req.body.order, size, etc
 this post is responsible for any post made to this endpiont, we're making a postreq to 
 this endpoint from your script
